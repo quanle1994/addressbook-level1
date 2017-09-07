73f02d8a4bf0d6db15d7ab51ccc7b2c0ecd7a046
quanle1994 <quan_le@u.nus.edu>
2017-09-07 13:44:55 +0800

    update help menu

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -106,7 +106,7 @@ public class AddressBook {
     private static final String COMMAND_ADD_EXAMPLE = COMMAND_ADD_WORD + " John Doe p/98765432 e/johnd@gmail.com";
 
     private static final String COMMAND_EDIT_WORD = "edit";
-    private static final String COMMAND_EDIT_DESC = "Edit a person's particular in the address book";
+    private static final String COMMAND_EDIT_DESC = "Edit a person's particular in the Address Book";
     private static final String COMMAND_EDIT_PARAMETERS = "NAME "
             + PERSON_DATA_PREFIX_PHONE + "PHONE_NUMBER "
             + PERSON_DATA_PREFIX_EMAIL + "EMAIL";
